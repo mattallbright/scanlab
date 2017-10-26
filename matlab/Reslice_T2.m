8a6a24f33944a8134b6a99999eafb4142a07375a
Matthew Allbright <allbrigh@email.arizona.edu>
2017-10-26 14:38:32 -0700

    Reslice_T2
    * Removed function

--- matlab/Reslice_T2.m
+++ matlab/Reslice_T2.m
@@ -1,16 +1,10 @@
-function Reslice_T2()
+nrun = 1; % enter the number of runs here
+jobfile = {'/opt/cloud/staging/matlab/Reslice_T2_job.m'};
+jobs = repmat(jobfile, 1, nrun);
+inputs = cell(0, nrun);
 
-    % PATHINPUT = fullpath;
-    % PARTIDINPUT = partID;
-
-    nrun = 1; % enter the number of runs here
-    jobfile = {'/opt/cloud/staging/matlab/Reslice_T2_job.m'};
-    jobs = repmat(jobfile, 1, nrun);
-    inputs = cell(0, nrun);
-
-    for crun = 1:nrun
-    end
-
-    spm('defaults', 'FMRI');
-    spm_jobman('run', jobs, inputs{:});
+for crun = 1:nrun
 end
+
+spm('defaults', 'FMRI');
+spm_jobman('run', jobs, inputs{:});
