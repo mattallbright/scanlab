8ecf4ef0b6466650812bbddba8f178ef923a7e5f
Matthew Allbright <allbrigh@email.arizona.edu>
2017-10-26 13:37:28 -0700

    MATLAB
    * Morons united

--- matlab/Reslice_T2.m
+++ matlab/Reslice_T2.m
@@ -1,13 +1,13 @@
-function garrfunc()
+function Reslice_T2(fullpath,partID)
 
-    % PATHINPUT = fullpath;
-    % PARTIDINPUT = partID;
+    PATHINPUT = fullpath;
+    PARTIDINPUT = partID;
 
-    % nrun = X; % enter the number of runs here
-    % jobfile = {'/opt/cloud/staging/matlab/Reslice_T2_job.m'};
-    % jobs = repmat(jobfile, 1, nrun);
-    % inputs = cell(0, nrun);
+    nrun = X; % enter the number of runs here
+    jobfile = {'/opt/cloud/staging/matlab/Reslice_T2_job.m'};
+    jobs = repmat(jobfile, 1, nrun);
+    inputs = cell(0, nrun);
 
-    % spm('defaults', 'FMRI');
-    % spm_jobman('run', jobs, inputs{:});
+    spm('defaults', 'FMRI');
+    spm_jobman('run', jobs, inputs{:});
 end
