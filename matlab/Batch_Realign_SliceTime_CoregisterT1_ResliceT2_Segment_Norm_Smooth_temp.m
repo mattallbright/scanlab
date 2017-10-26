3f8fb192e81d4e562d58f6c72f47e70191273443
Matthew Allbright <allbrigh@email.arizona.edu>
2017-10-26 13:20:32 -0700

    MATLAB
    * Testing files for end types

--- matlab/Batch_Realign_SliceTime_CoregisterT1_ResliceT2_Segment_Norm_Smooth_temp.m
+++ matlab/Batch_Realign_SliceTime_CoregisterT1_ResliceT2_Segment_Norm_Smooth_temp.m
@@ -1,16 +1,14 @@
 function garrfunc(fullpath, partID)
 
-global PATHINPUT = fullpath;
-global PARTIDINPUT = partID;
+    global PATHINPUT = fullpath;
+    global PARTIDINPUT = partID;
 
-nrun = X; % enter the number of runs here
-jobfile = {'/opt/cloud/staging/matlab'};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(0, nrun);
-for crun = 1:nrun
-end
-spm('defaults', 'FMRI');
-spm_jobman('run', jobs, inputs{:});
+    nrun = X; % enter the number of runs here
+    jobfile = {'/opt/cloud/staging/matlab'};
+    jobs = repmat(jobfile, 1, nrun);
+    inputs = cell(0, nrun);
 
+    spm('defaults', 'FMRI');
+    spm_jobman('run', jobs, inputs{:});
 
 end
