58b730e4c4ca631068324a65ac0a87bc46b5c368
Matthew Allbright <allbrigh@email.arizona.edu>
2017-10-26 13:40:56 -0700

    Reslice_T2.m
    * Fixed an issue with number of runs.

--- matlab/Reslice_T2.m
+++ matlab/Reslice_T2.m
@@ -3,7 +3,7 @@ function Reslice_T2(fullpath,partID)
     PATHINPUT = fullpath;
     PARTIDINPUT = partID;
 
-    nrun = X; % enter the number of runs here
+    nrun = 1; % enter the number of runs here
     jobfile = {'/opt/cloud/staging/matlab/Reslice_T2_job.m'};
     jobs = repmat(jobfile, 1, nrun);
     inputs = cell(0, nrun);
